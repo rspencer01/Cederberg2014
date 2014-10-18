265d8644af26692605239ac539238be322807edd
Robert Spencer <robert.spencer94@gmail.com>
2014-10-18 23:16:02 +0200

    Writes digit to display

--- sseg.c
+++ sseg.c
@@ -6,9 +6,35 @@
  */ 
 #include "sseg.h"
 #include "gpio.h"
+#include "utils.h"
 
+// The bits for the display select
 const unsigned char SSEG_SELECT[3] = {0b01000000,0b00010000,0b00000010};
+
+// The numbers
+const unsigned char SSEG_NUMBERS[10] = {SSEG_ZERO,SSEG_ONE,SSEG_TWO,SSEG_THREE,SSEG_FOUR,SSEG_FIVE,SSEG_SIX,SSEG_SEVEN,SSEG_EIGHT,SSEG_NINE};
+
+// The values in each display
+int displays[3] = {0,0,0};
+
+// The currently strobed display number
 int currentDisplay = 0;
+
+// Writes the correct values to the correct ports.  Don't mess with this
+// unless you know exactly what you are doing.
+void writeDisplay(int segmentNumber)
+{
+  portB = setNthBit(portB,(displays[segmentNumber] & 0x02) << 5, 6);
+  portB = setNthBit(portB,(displays[segmentNumber] & 0x04) >> 2, 0);
+  portB = setNthBit(portB,(displays[segmentNumber] & 0x10) << 3, 7);
+
+  portC = setNthBit(portC,(displays[segmentNumber] & 0x20)     , 5);
+  portC = setNthBit(portC,(displays[segmentNumber] & 0x80) >> 3, 4);
+
+  portD = setNthBit(portD,(displays[segmentNumber] & 0x01)     , 0);
+  portD = setNthBit(portD,(displays[segmentNumber] & 0x08) << 2, 5);
+  portD = setNthBit(portD,(displays[segmentNumber] & 0x40) << 1, 7);
+}
 
 // Strobes the display.
 void updateDisplay()
@@ -17,5 +43,16 @@ void updateDisplay()
   currentDisplay++;
   currentDisplay%=3;
   SSEG_SELECT_PORT |= SSEG_SELECT[currentDisplay];
+  writeDisplay(currentDisplay);
   setPorts();
+}
+
+// Writes a three digit integer to the display.
+void writeNumber(int n)
+{
+  displays[0] = SSEG_NUMBERS[n%10];
+  n/=10;
+  displays[1] = SSEG_NUMBERS[n%10];
+  n/=10;
+  displays[2] = SSEG_NUMBERS[n%10];
 }
--- sseg.h
+++ sseg.h
@@ -4,17 +4,42 @@
  * Created: 2014/03/21 12:18:07 PM
  *  Author: Robert
  */ 
-
-
 #ifndef SSEG_H_
 #define SSEG_H_
+
+// The seven segment displays are set out as follows:
+//
+//        0
+//      5   1
+//        6
+//      4   2
+//        3   7
+//
+// We only show one at a time.  The rest are strobed
+// one after another fast enough to appear static to
+// the human eye.
 
 // The port on which the sseg select pins reside
 #define SSEG_SELECT_PORT portD
 // A mask for which pins are sseg select pins
 #define SSEG_SELECT_MASK 0b01010010
-extern const unsigned char SSEG_SELECT[3];
 
+// Numbers
+#define SSEG_ZERO  0b11000000
+#define SSEG_ONE   0b11111001
+#define SSEG_TWO   0b10100100
+#define SSEG_THREE 0b10110000
+#define SSEG_FOUR  0b10011001
+#define SSEG_FIVE  0b10010010
+#define SSEG_SIX   0b10000010
+#define SSEG_SEVEN 0b11111000
+#define SSEG_EIGHT 0b10000000
+#define SSEG_NINE  0b10010000
+
+// Performs the strobing
 void updateDisplay();
+
+// Writes a number to the displays
+void writeNumber(int);
 
 #endif /* SSEG_H_ */
